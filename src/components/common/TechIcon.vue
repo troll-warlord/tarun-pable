0b2c507e32a2c5a442fe67a72a3c4781bd576984
Tarun Pable <34676646+troll-warlord@users.noreply.github.com>
2026-02-12 11:26:39 +0530

    Update TechIcon.vue

--- src/components/common/TechIcon.vue
+++ src/components/common/TechIcon.vue
@@ -21,7 +21,7 @@ const getIconUrl = (slug) => {
 
 <template>
   <div class="tech-item-base group/logo hover:-translate-y-2 shrink-0">
-    <img :src="getIconUrl(slug.toLowerCase())" class="tech-icon-base" :alt="alt || slug" />
+    <img :src="getIconUrl(slug.toLowerCase())" loading="lazy" class="tech-icon-base" :alt="alt || slug" />
   </div>
   <div v-if="name" class="flex flex-col md:items-center">
     <span class="text-zinc-200 font-bold font-mono md:text-lg uppercase tracking-wider group-hover:text-emerald-400 transition-colors">
@@ -29,4 +29,5 @@ const getIconUrl = (slug) => {
     </span>
   </div>
 </template>
+
 
